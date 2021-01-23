432676ab68b8cfb7bce83b5a96c12bdc9edb546d
Naveed Sufi <nhsufi@gmail.com>
2021-01-23 14:03:52 -0500

    Sequelize config

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+module.exports = {
+  development: {
+    url: process.env.DB_CONNECTION_STRING,
+    dialect: "postgres",
+  },
+  production: {
+    url: process.env.DB_CONNECTION_STRING,
+    dialect: "postgres",
+  },
+};
